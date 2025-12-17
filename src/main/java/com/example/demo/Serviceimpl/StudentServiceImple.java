2eb2c056e95f49e4e58f8214d0763de02ccf3e1b
bala-amypo <bala@amypo.in>
2025-12-17 10:01:17 +0000

    User commit

--- src/main/java/com/example/demo/Serviceimpl/StudentServiceImple.java
+++ src/main/java/com/example/demo/Serviceimpl/StudentServiceImple.java
@@ -1,10 +1,20 @@
-package com.example.demo.service;
+package com.example.aiml.Serviceimpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import com.example.demo.entity.Student;
+import com.example.aiml.entity.Student;
+import com.example.aiml.repository.StudentRepository;
+import com.example.aiml.service.StudentService;
 
+@Service
+public class StudentServiceImple implements StudentService{
+    @Autowired
+    StudentRepository studentRepository;
+    
 
-public interface StudentService {
-    Student saveStudent(Student student);
-
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+    
 }
--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,20 +1,10 @@
-package com.example.demo.Serviceimpl;
+package com.example.demo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Student;
-import com.example.demo.repository.StudentRepository;
-import com.example.demo.service.StudentService;
 
-@Service
-public class StudentServiceImple implements StudentService{
-    @Autowired
-    StudentRepository studentRepository;
-    
 
-    public Student saveStudent(Student student) {
-        return studentRepository.save(student);
-    }
-    
+public interface StudentService {
+    Student saveStudent(Student student);
+
 }
