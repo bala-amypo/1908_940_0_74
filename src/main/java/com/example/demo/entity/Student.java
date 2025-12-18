540b388c93a610a88b659d066160f69a58b40cfb
bala-amypo <bala@amypo.in>
2025-12-18 09:01:58 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -20,7 +20,7 @@ public class Student {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     
 
-    private int id;
+    private Long id;
     @NotBlank(message="Name filed cannot be empty")
     @Size(min=3,max=20,message="The user name must be min of 3 and max of 20 character")
     private String name;
@@ -35,12 +35,10 @@ public class Student {
     //@Max
     //@Pattern(reg)
     
-    private LocalDate dob;
-    private float cgpa;
-    public int getId() {
+    public Long getId() {
         return id;
     }
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
     public String getName() {
@@ -55,24 +53,12 @@ public class Student {
     public void setEmail(String email) {
         this.email = email;
     }
-    public LocalDate getDob() {
-        return dob;
-    }
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-    public float getCgpa() {
-        return cgpa;
-    }
-    public void setCgpa(float cgpa) {
-        this.cgpa = cgpa;
-    }
-    public Student(int id, String name, String email, LocalDate dob, float cgpa) {
+   
+
+    public Student(Long id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
-        this.dob = dob;
-        this.cgpa = cgpa;
     }
     public Student() {
     }
